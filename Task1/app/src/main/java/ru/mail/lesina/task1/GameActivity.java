2f126a20040d78ec393e7ff9eafa429deea0f321
lesina <lesha_lesovoy@mail.ru>
2017-10-10 23:46:59 +0300

    UPDATE according to neccessarity

--- Task1/app/src/main/java/ru/mail/lesina/task1/GameActivity.java
+++ Task1/app/src/main/java/ru/mail/lesina/task1/GameActivity.java
@@ -9,8 +9,10 @@ import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
-    public int counter;
-    public boolean isStop;
+    public int counter = 1;
+    public boolean isStop = false;
+    protected long totalTime = 11 * 1000;
+    public long currentTime = totalTime;
     Button button;
     TextView textView;
     CountDownTimer timer;
@@ -20,40 +22,90 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        isStop = false;
-        button= (Button) findViewById(R.id.button);
-        counter = 1;
-        textView= (TextView) findViewById(R.id.textView2);
-        timer = new CountDownTimer(101000, 1000){
-            public void onTick(long millisUntilFinished){
-                textView.setText(num.toString(counter));
-                counter++;
-            }
-            public  void onFinish(){
-                button.setText("start");
-                isStop = false;
-                counter = 1;
-                textView.setText("");
-            }
-        };
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView2);
+        timer = newTimer(currentTime, 1000);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!isStop) {
-                    button.setText("stop");
+                if (isStop) {
+                    button.setText(getString(R.string.button_start));
+                    isStop = false;
+                    counter = 1;
+                    currentTime = 10 * 1000;
+                    textView.setText("");
+                    timer.cancel();
+                } else {
+                    button.setText(getString(R.string.button_stop));
                     isStop = true;
                     timer.start();
                 }
-                else {
-                    button.setText("start");
-                    isStop = false;
-                    counter = 1;
-                    textView.setText("");
-                    timer.cancel();
-                }
             }
         });
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(getString(R.string.count), counter);
+        outState.putBoolean(getString(R.string.button), isStop);
+        outState.putString(getString(R.string.text), textView.getText().toString());
+        outState.putString(getString(R.string.button_text), button.getText().toString());
+        outState.putLong(getString(R.string.current_time), totalTime - (counter - 1) * 1000);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt(getString(R.string.count));
+        isStop = savedInstanceState.getBoolean(getString(R.string.button));
+        textView.setText(savedInstanceState.getString(getString(R.string.text)));
+        button.setText(savedInstanceState.getString(getString(R.string.button_text)));
+        currentTime = savedInstanceState.getLong(getString(R.string.current_time));
+        timer = newTimer(currentTime, 1000);
+        if (isStop) {
+            timer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (isStop) {
+            currentTime = totalTime - (counter - 1) * 1000;
+            timer.cancel();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        if (isStop) {
+            timer = newTimer(currentTime, 1000);
+            timer.start();
+        }
+        super.onStart();
+    }
+
+    public CountDownTimer newTimer(long time, long miliseccondsSTOP) {
+        return new CountDownTimer(time, miliseccondsSTOP) {
+            public void onTick(long millisUntilFinished) {
+                textView.setText(num.toString(counter));
+                counter++;
+            }
+
+            public void onFinish() {
+                button.setText(getString(R.string.button_start));
+                isStop = false;
+                counter = 1;
+                currentTime = 10 * 1000;
+                textView.setText("");
+            }
+        };
     }
 }
--- Task1/app/src/main/java/ru/mail/lesina/task1/MainActivity.java
+++ Task1/app/src/main/java/ru/mail/lesina/task1/MainActivity.java
@@ -7,29 +7,82 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     CountDownTimer timer;
+    public long totalTime = 3 * 1000;
+    public long currentTime = totalTime;
+    public int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timer = new CountDownTimer(3000, 1000){
-            @Override
-            public void onTick(long l) {
+        timer = newTimer(currentTime, 1000);
+    }
 
-            }
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 
-            public  void onFinish(){
-                createGame();
-            }
-        }.start();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        timer = newTimer(currentTime, 1000);
+        onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        currentTime = totalTime - count * 1000;
+        timer.cancel();
+    }
+
+    @Override
+    protected void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        timer.start();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(getString(R.string.count), count);
+        outState.putLong(getString(R.string.current_time), totalTime - count * 1000);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt(getString(R.string.count));
+        currentTime = savedInstanceState.getLong(getString(R.string.current_time));
+        timer = newTimer(currentTime, 1000);
     }
 
     public void createGame() {
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
+    }
+
+    public CountDownTimer newTimer(long time, long miliseccondsSTOP) {
+        return new CountDownTimer(time, miliseccondsSTOP) {
+            public void onTick(long millisUntilFinished) {
+                count++;
+            }
+
+            public void onFinish() {
+                createGame();
+                onDestroy();
+            }
+        };
     }
 }
